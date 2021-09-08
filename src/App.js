97bae9ff36f9f36abef1069513fa4374a64fae3d
apple <apple@apples-MacBook-Pro.local>
2021-09-08 13:44:59 +0300

    styled component bureger menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Burger, Menu } from './components';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { useOnClickOutside } from './hooks';
+import FocusLock from 'react-focus-lock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [open, setOpen] = useState(false);
+	const node = useRef();
+	const menuId = 'main-menu';
+	useOnClickOutside(node, () => setOpen(false));
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<GlobalStyles />
+				<div ref={node}>
+					<FocusLock disabled={!open}>
+						<Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+						<Menu open={open} setOpen={setOpen} id={menuId} />
+					</FocusLock>
+				</div>
+				<div>
+					<h1>Hello. This is burger menu tutorial</h1>
+					<img
+						src='https://image.flaticon.com/icons/svg/2016/2016012.svg'
+						alt='burger icon'
+					/>
+					<small>Icon made by Freepik from www.flaticon.com</small>
+				</div>
+			</>
+		</ThemeProvider>
+	);
 }
-
 export default App;
